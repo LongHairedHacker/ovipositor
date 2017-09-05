fb373c866cd859a36dc3794dfd9dfa8607bc2fa0
LongHairedHacker <sebastian@sebastians-site.de>
2017-09-05 22:08:19 +0200

    Added more hashtags

--- main.py
+++ main.py
@@ -19,6 +19,6 @@ if __name__ == '__main__':
 
 	while True:
 		stream = tweepy.Stream(auth = api.auth, listener=RetweetingStreamListener(api))
-		stream.filter(track=['#zaunei2017', '@zaunei2017'])
+		stream.filter(track=['#tschunkcyberparty', '#tcp2017', '#zaunei2017', '@zaunei2017'])
 		print("[Error] Something went wrong reconnecting")
 		sleep(10.0)
